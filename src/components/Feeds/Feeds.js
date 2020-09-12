ac58e63d266c0983bb552275adb7c1c63f78932a
vyshnav.sp <vyshnav.sp@zohocorp.com>
2020-09-12 13:33:35 +0530

    Create Feeds container

---
+++ src/components/Feeds/Feeds.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Feed from './Feed.js/Feed';
+
+function Feeds() {
+	return (
+		<div className="feeds row span-100-6 p-05 mt-05">
+			<Feed />
+			<Feed />
+			<Feed />
+		</div>
+	)
+}
+
+export default Feeds;
