48be1cd650106e453a3572d425047128634532a1
vyshnav.sp <vyshnav.sp@zohocorp.com>
2020-09-12 13:18:58 +0530

    Include search and navs

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import Search from '../../components/Search/Search';
+import Navs from '../../components/Navs/Navs';
 
 function Header() {
 	return (
-		<header className="row span-100-12">
+		<header className="row span-100-12 p-1 vertical-center card">
 			<h3 className="row span-100-3">picshare</h3>
 			<div className="row span-100-4">
-				{/* Add Search */}
+				<Search />
 			</div>
 			<div className="row span-100-5">
-				{/* Add navs */}
+				<Navs />
 			</div>
 		</header>
 	)
