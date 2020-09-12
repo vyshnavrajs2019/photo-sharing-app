6669b73084334dca8312e670abdf1291806f1658
vyshnav.sp <vyshnav.sp@zohocorp.com>
2020-09-12 12:16:05 +0530

    Add Header and Main containers

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from './Header/Header';
+import Main from './Main/Main';
 
 function App() {
 	return (
-		<div className="App">
-			
-		</div>
+		<React.Fragment>
+			<Header />
+			<Main />
+		</React.Fragment>
 	);
 }
 
