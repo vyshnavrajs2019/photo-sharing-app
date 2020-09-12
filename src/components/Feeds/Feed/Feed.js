d3b77469648b3dd57b61373103e3cf71821bbe9b
vyshnav.sp <vyshnav.sp@zohocorp.com>
2020-09-12 13:34:57 +0530

    Create Feed component

---
+++ src/components/Feeds/Feed/Feed.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Feed extends Component {
+	render() {
+		return (
+			<div className="feed row span-100-12 p-1 card mb-1">
+				
+			</div>
+		)
+	}
+}
+
+export default Feed;
