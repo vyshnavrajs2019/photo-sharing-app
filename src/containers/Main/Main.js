00b76a88eb69bb58e7519796c0c757d930a510f4
vyshnav.sp <vyshnav.sp@zohocorp.com>
2020-09-12 13:33:13 +0530

    Include Feeds and Profile routes

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Feeds from '../../components/Feeds/Feeds';
 
 function Main() {
 	return (
-		<main>
-			
+		<main className="row span-100-12 horizontal-center">
+			<Switch>
+				<Route exact path="/" component={Feeds} />
+				<Route exact path="/profile/:username" component={Feeds} />
+			</Switch>
 		</main>
 	)
 }
