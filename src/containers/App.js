f645215f1c999070407ef710b63b1dfaab858105
vyshnav.sp <vyshnav.sp@zohocorp.com>
2020-09-12 13:18:39 +0530

    Replace React.Fragment with BrowserRouter

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './Header/Header';
 import Main from './Main/Main';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
 	return (
-		<React.Fragment>
+		<BrowserRouter>
 			<Header />
 			<Main />
-		</React.Fragment>
+		</BrowserRouter>
 	);
 }
 
