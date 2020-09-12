8158e5484a77f85963cd96d184b74e803b8556ca
vyshnav.sp <vyshnav.sp@zohocorp.com>
2020-09-12 12:19:37 +0530

    Add site name and space for search and navs

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 function Header() {
 	return (
-		<header>
-			
+		<header className="row span-100-12">
+			<h3 className="row span-100-3">picshare</h3>
+			<div className="row span-100-4">
+				{/* Add Search */}
+			</div>
+			<div className="row span-100-5">
+				{/* Add navs */}
+			</div>
 		</header>
 	)
 }
